8385b4f0fc41f2f6cf9bbadf113f79f8c28fec03
Philip Rettig <phire844@student.liu.se>
2022-11-14 15:09:42 +0100

    Loaded data

--- ARIMA/predict.R
+++ ARIMA/predict.R
@@ -1,9 +1,20 @@
 library("readxl")
 library("httpgd")
 
-my_data <- read_excel("./data/klein.xls")
-my_data
+my_data <- read_excel("./data/commodities.xls")
+my_data <- as.ts(my_data)
 
-plot(my_data)
+plot.ts(my_data)
+table(my_data)
+df
+
+View(my_data)
+
+firstDiff <- diff(my_data,lag=1, differences=1)
+secondDiff <- diff(my_data,lag=1, differences=2)
+thirdDiff <- diff(my_data,lag=1, differences=3)
+
+acf(my_data)
+pacf(my_data)
 
 
