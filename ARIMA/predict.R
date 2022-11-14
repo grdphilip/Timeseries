dc56844f9ef2d64b106ec6a6db31e464ad73d007
Philip Rettig <phire844@student.liu.se>
2022-11-14 22:26:34 +0100

    Update

--- ARIMA/predict.R
+++ ARIMA/predict.R
@@ -1,18 +1,30 @@
 library("readxl")
 library("httpgd")
+library("dplyr")
+library("ggplot2")
 
-my_data <- read_excel("./data/commodities.xls")
-my_data <- as.ts(my_data)
+my_data <- read_excel("./data/commodities-1.xls")
+my_data <- as.ts(my_data[,"Sugar price"])
 
-plot.ts(my_data)
+
+plot(my_data[, "Sugar price"])
+plot.ts(my_data[,"Sugar price"], col="blue")
 table(my_data)
-df
+my_data[, "Sugar price"]
 
 View(my_data)
 
-firstDiff <- diff(my_data,lag=1, differences=1)
-secondDiff <- diff(my_data,lag=1, differences=2)
-thirdDiff <- diff(my_data,lag=1, differences=3)
+#Take first, second and third lag difference of data
+firstDiff <- diff(my_data[,"Sugar price"],lag=1, differences=1)
+secondDiff <- diff(my_data[,"Sugar price"],lag=1, differences=2)
+thirdDiff <- diff(my_data[,"Sugar price"],lag=1, differences=3)
+plot.ts(thirdDiff, col="darkgreen")
+
+ggplot(my_data, aes(x=my_data[,"Date"], y=my_data[,"Sugar price"])) +
+       geom_line() 
+       
+
+
 
 acf(my_data)
 pacf(my_data)
