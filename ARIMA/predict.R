b6ba83fdf78f88df96f4d6e0f912e83c8ec7f77f
Philip Rettig <phire844@student.liu.se>
2022-11-22 14:00:43 +0100

    ARIMA Preparation

--- ARIMA/predict.R
+++ ARIMA/predict.R
@@ -2,31 +2,39 @@ library("readxl")
 library("httpgd")
 library("dplyr")
 library("ggplot2")
+library("forecast")
+library("seasonal")
 
 my_data <- read_excel("./data/commodities-1.xls")
-my_data <- as.ts(my_data[,"Sugar price"])
+my_data <- ts(my_data, start=c(1960, 1), end = c(2022, 10), frequency = 12)
+logged <- log(my_data)
+data_reserv <- my_data 
+colnames(data_reserv) <- "Sugar price"
 
-
-plot(my_data[, "Sugar price"])
-plot.ts(my_data[,"Sugar price"], col="blue")
+plot(my_data, ylab="Sugar price", col="blue")
 table(my_data)
 my_data[, "Sugar price"]
 
 View(my_data)
 
 #Take first, second and third lag difference of data
-firstDiff <- diff(my_data[,"Sugar price"],lag=1, differences=1)
-secondDiff <- diff(my_data[,"Sugar price"],lag=1, differences=2)
-thirdDiff <- diff(my_data[,"Sugar price"],lag=1, differences=3)
-plot.ts(thirdDiff, col="darkgreen")
+firstlogDiff <- diff(log(my_data[,"Sugar price"]),lag=1, differences=1)
+secondlogDiff <- diff(log(my_data[,"Sugar price"]),lag=1, differences=2)
+thirdlogDiff <- diff(log(my_data[,"Sugar price"]),lag=1, differences=3)
+diff.ts <- diff(my_data, lag=1, differences=1)
+plot.ts(diff.ts, col="#a19f2d", main = "Diff")
 
-ggplot(my_data, aes(x=my_data[,"Date"], y=my_data[,"Sugar price"])) +
-       geom_line() 
-       
+summary(my_data)
+Acf(my_data, lag.max = 20)
+Pacf(my_data, lag.max = 20)
+#Kolla diffad data
+Acf(diff.ts, lag.max = 20, main="Acf diffdata")
+Pacf(diff.ts, lag.max = 20, main="Pacf diffdata")
+#Verkar som att datan är 
 
 
 
-acf(my_data)
-pacf(my_data)
+
+
 
 
